20f1899805f71fbc2a6a66d4e8dc08831dbedc27
Rayner Susanto <rayner.susanto0@gmail.com>
2025-02-20 23:32:08 -0700

    Updated landing page skeleton

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,47 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-6 items-center row-start-1 sm:items-center max-w-7xl">
+        <div className="flex items-center flex-col sm:flex-row">
+          <h1 className="text-5xl text-center"> Welcome to Our Website </h1>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex items-center flex-col sm:flex-row">
+          <h2 className="text-xl text-center"> At Pandu Web, we prioritize your convenience and satisfaction over any other factor. We believe that software solutions must be beneficial for our clients and give them an easier, not a headache. </h2> 
+        </div>
+
+        <div className="flex items-center flex-col sm:flex-row my-10">
+          <button className="bg-gray-500 text-white hover:bg-gray-900 focus:ring-gray-700 px-20 py-4 text-base rounded-lg justify-center">
+            Sign Up
+          </button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between sm:flex-row w-full">
+            <h3 className="text-lg flex-1 text-start"> Customer Support #1 </h3>
+            <h3 className="text-lg flex-1 text-center"> Understand YOUR Needs</h3>
+            <h3 className="text-lg flex-1 text-end"> Lovable Products </h3>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
     </div>
   );
 }
